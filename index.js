5c5a8b266cbffc3971e91879a9cb1b315bd931bf
Benson Mwangi <kimaniben@gmail.com>
2023-09-24 01:16:16 -0400

    menu updates

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ document.querySelector('#darkModeToggle').addEventListener('click', () => {
     darkMode = !darkMode;
     document.body.classList.toggle('dark-mode', darkMode);
 });
+let menu = document.getElementById("mobileMenu");
+document.getElementById("menuToggle").addEventListener("click",function(){
+    if (menu.style.display === "none"){
+        menu.style.display = "block";
+    } else {
+        menu.style.display ="none";
+    }
+});
